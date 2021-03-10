12132b6097d72e316ce05fa731171f71d9d5455b
thisisharrison <harrison.lau@outlook.com>
2021-03-10 23:41:21 +0800

    finish memoization

--- Data Structures and Algorithms/memoization_project/lib/leet_code_518.js
+++ Data Structures and Algorithms/memoization_project/lib/leet_code_518.js
@@ -1,3 +1,60 @@
 // Work through this problem on https://leetcode.com/problems/coin-change-2/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
+// You are given coins of different denominations and a total amount of money. 
+// Write a function to compute the number of combinations that make up that amount. 
+// You may assume that you have infinite number of each kind of coin.
+
+// Example 1:
+// Input: amount = 5, coins = [1, 2, 5]
+// Output: 4
+// Explanation: there are four ways to make up the amount:
+// 5=5
+// 5=2+2+1
+// 5=2+1+1+1
+// 5=1+1+1+1+1
+
+// Example 2:
+// Input: amount = 3, coins = [2]
+// Output: 0
+// Explanation: the amount of 3 cannot be made up just with coins of 2.
+
+// Example 3:
+// Input: amount = 10, coins = [10] 
+// Output: 1
+
+
+// build tree, each level explore quantities of coin
+// if path returns 1, there's 1 way to make change, if 0, there's 0 way
+function change(amount, coins) {
+  // if amount = 0, then there's 1 way to give them back their change
+  if (amount === 0) return 1;
+
+  // take a coin and see if we get an amount
+  let currCoin = coins[coins.length - 1]
+  let numWays = 0
+
+  for (let qty = 0; qty * currCoin < amount; qty++) {
+    // accumulate the ways to make change
+    numWays += change(amount - qty * currCoin, coins.slice(0, -1))
+  }
+  
+  return numWays;
+}
+
+function memo_change(amount, coins, memo={}) {
+  // create key that includes data on amount and varied coins array
+  let key = amount + '-' + coins;
+  if (key in memo) return memo[key];
+  if (amount === 0) return 1;
+
+  let currCoin = coins[coins.length - 1]
+  let numWays = 0
+
+  for (let qty = 0; qty * currCoin <= amount; qty++) {
+    numWays += change(amount - qty * currCoin, coins.slice(0, -1), memo)
+  }
+  
+  memo[key] = numWays;
+  return memo[key];
+}
--- Data Structures and Algorithms/memoization_project/lib/problems.js
+++ Data Structures and Algorithms/memoization_project/lib/problems.js
@@ -16,7 +16,11 @@
 // lucasNumberMemo(41)  // => 370248451
 // lucasNumberMemo(42)  // => 599074578
 function lucasNumberMemo(n, memo = {}) {
-
+  if (n in memo) return memo[n];
+  if (n === 0) return 2;
+  if (n === 1) return 1;
+  memo[n] = lucasNumberMemo(n - 2, memo) + lucasNumberMemo(n - 1, memo);
+  return memo[n];
 }
 
 
@@ -34,7 +38,22 @@ function lucasNumberMemo(n, memo = {}) {
 // minChange([1, 5, 10, 25], 15)    // => 2, because 10 + 5 = 15
 // minChange([1, 5, 10, 25], 100)   // => 4, because 25 + 25 + 25 + 25 = 100
 function minChange(coins, amount, memo = {}) {
+  // only amount changes coins is fixed 
+  if (amount in memo) return memo[amount];
+  if (amount === 0) return 0;
+  let numCoins = [];
+  coins.forEach(coin => {
+    if (coin <= amount ) {
+      // returns 0 when we hit base case, so we +1 to count the coin we're using in this loop
+      numCoins.push(minChange(coins, amount - coin, memo) + 1);
+    }
+  });
+  // brute force
+  // return Math.min(...numCoins);
 
+  // memoize the return value
+  memo[amount] = Math.min(...numCoins);
+  return memo[amount];
 }
 
 
